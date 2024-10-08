c12786653700922acb3c03fa5c360cafc95c9ceb
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-10-08 09:09:14 +0200

    n-dut | variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'hsl(var(--background), 1)',
+        foreground: 'hsl(var(--foreground), 1)',
+        componentsBg: 'hsl(var(--components-bg), 1)',
+        btnBg: 'hsl(var(--btn-bg), 1)',
+        btnFg: 'hsl(var(--btn-fg), 1)',
+        footerBg: 'hsl(var(--footer-bg), 1)',
+        footerFg: 'hsl(var(--footer-fg), 1)',
+        cardDate: 'hsl(var(--card-date), 1)',
+        cardCreatePrimary: 'hsl(var(--card-create-primary), 1)',
+        cardCreateSecondary: 'hsl(var(--card-create-secondary), 1)',
+        cardResearchPrimary: 'hsl(var(--card-research-primary), 1)',
+        cardResearchSecondary: 'hsl(var(--card-research-secondary), 1)',
+        btnPublic: 'hsl(var(--btn-public), 1)',
+        btnPrivate: 'hsl(var(--btn-private), 1)',
       },
     },
   },
