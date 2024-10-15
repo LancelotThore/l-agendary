837f9d35f5a8f290730f6e5186a2d77a06c3a9ae
Lancelot Thoré <144225580+LancelotThore@users.noreply.github.com>
2024-10-15 11:44:33 +0200

    headerCard

--- src/components/ui/eventHeader.tsx
+++ src/components/ui/eventHeader.tsx
@@ -4,16 +4,16 @@ import { Button } from '@/components/ui/button';
 
 const EventHeader = () => {
     return (
-        <div className='flex w-full relative overflow-hidden items-end px-3 rounded-3xl'>
-            <img src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card" className='absolute z-[-10] w-full left-0 rounded-3xl'/>
-            <div className='w-full flex flex-col mt-48'>
-                <div className='flex items-center gap-2 mb-3'>
+        <div className='flex w-full relative overflow-hidden items-end px-3 rounded-3xl bg-gradient-to-b from-transparent from-50% to-primary text-primary-foreground'>
+            <img src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card" className='absolute z-[-10] w-full h-full left-0 rounded-3xl'/>
+            <div className='w-full flex flex-col mt-48 mb-6 gap-2'>
+                <div className='flex items-center gap-2 mb-1'>
                     <h1 className='text-xl font-bold'>Pique-nique Festif</h1>
                     <Button variant='public' size='sm'>Public<LockOpenIcon className='w-4 ml-2' /></Button>
                 </div>
-                <span className='flex'><Clock className='w-5'/><p>Samedi 17 août 2029 - 11h à 14h</p></span>
-                <span className='flex'><LocationOn className='w-5'/><p>Central Park, New York</p></span>
-                <span className='flex'><PeopleFill className='w-5'/><p>24 participants</p></span>
+                <span className='flex items-center gap-1.5'><Clock className='w-5'/><p>Samedi 17 août 2029 - 11h à 14h</p></span>
+                <span className='flex items-center gap-1.5'><LocationOn className='w-5'/><p>Central Park, New York</p></span>
+                <span className='flex items-center gap-1.5'><PeopleFill className='w-5'/><p>24 participants</p></span>
             </div>
         </div>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,13 +47,12 @@ const config: Config = {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
-  			}
+  			},
+			gradients: {
+				'primary': 'var(--gradient-primary)',
+				'secondary': 'var(--gradient-secondary)',
+			}
   		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
