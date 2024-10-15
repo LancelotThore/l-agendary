2750a753f1dbe255f47811ba15cb2f5b45fabd53
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-10-15 14:36:18 +0200

    navbar

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -15,12 +15,12 @@ export function Navbar() {
         <nav className="bg-secondary text-primary">
             <div className="flex items-center justify-between mx-auto p-6 shadow-md">
                 <div className="flex items-center space-x-10 rtl:space-x-reverse">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <a href="/" className="flex items-center">
                         <Logo className="w-40" />
                     </a>
                     <a
                         href="/"
-                        className="hidden md:flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded hover:text-blue-700 font-medium text-lg"
+                        className="hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base"
                     >
                         <span>Rechercher</span>
                         <Search className="w-5 h-5" aria-hidden="true" />
@@ -34,19 +34,19 @@ export function Navbar() {
                     {isMenuOpen ? <Close className="w-8" /> : <MenuBurger className="w-8" />}
                 </button>
                 <div className="hidden md:block" id="navbar-default">
-                    <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-secondary">
-                        <div className="hidden md:flex items-center rtl:space-x-reverse">
+                    <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 rounded-lg md:flex-row md:mt-0 md:border-0 md:bg-secondary">
+                        <div className="hidden md:flex items-center">
                             <a
-                                href="/"
-                                className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium"
+                                href="/signup"
+                                className="flex items-center block py-2 px-3 rounded font-medium"
                             >
-                                <Button className="mt-2">S'enregistrer</Button>
+                                <Button className="mt-2 hover:bg-primary/70">S'enregistrer</Button>
                             </a>
                             <a
-                                href="/"
-                                className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium"
+                                href="/login"
+                                className="flex items-center block py-2 px-3 rounded font-medium"
                             >
-                                <Button className="mt-2">Se connecter</Button>
+                                <Button className="mt-2 hover:bg-primary/70">Se connecter</Button>
                             </a>
                         </div>
                     </ul>
@@ -74,7 +74,7 @@ export function Navbar() {
                     </li>
                     <li>
                         <a
-                            href="/"
+                            href="/signup"
                             className="flex items-center block px-3 py-1 rounded font-medium"
                         >
                             <Button className="mt-2">S'enregistrer</Button>
@@ -82,7 +82,7 @@ export function Navbar() {
                     </li>
                     <li>
                         <a
-                            href="/"
+                            href="/login"
                             className="flex items-center block px-3 py-1 rounded font-medium"
                         >
                             <Button className="mt-2">Se connecter</Button>
