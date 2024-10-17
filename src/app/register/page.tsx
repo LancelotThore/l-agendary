3a1dc1364d836b5f72c60dfa0697054bca3cb417
josueprlt <josue.perrault@etu.unilim.fr>
2024-10-17 16:14:46 +0200

    up

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,18 +1,52 @@
-import Image from "next/image";
 import { Raleway } from 'next/font/google';
-import RegisterLayout from './layout';
+import { Input } from "@/components/ui/input";
 
-const raleway = Raleway({
+const ralewaySemBold = Raleway({
   subsets: ['latin'],
-  weight: '900',
+  weight: '600',
   variable: '--font-raleway',
-})
+});
+
+const ralewayMedium = Raleway({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-raleway',
+});
 
 export default function RegisterPage() {
+  let inputs = [
+    {
+      id: 'name',
+      name: 'Nom',
+    },
+    {
+      id: 'firstname',
+      name: 'Prénom',
+    },
+    {
+      id: 'email',
+      name: 'Email',
+    },
+    {
+      id: 'password',
+      name: 'Mot de passe',
+    },
+  ]
   return (
-    <RegisterLayout>
-      <h1>Page de Registration</h1>
-      {/* Votre contenu de page ici */}
-    </RegisterLayout>
+    <div className={`${ralewayMedium.className} flex flex-col h-screen justify-center items-center text-xs`}>
+      <img className='w-24' src="./logo2.svg" alt="Logo Icon" />
+
+      <section className='w-9/12 shadow-md mt-5 p-4 border border-cardDate rounded-md'>
+        <h2 className={`${ralewaySemBold.className} text-base`}>Créer un compte</h2>
+
+        {inputs.map((input)=>(
+          <div className='mt-3 flex flex-col gap-1'>
+            <label htmlFor={input.id}>{input.name}</label>
+            <Input id={input.id} placeholder={input.name} className='text-xs text-cardDate' />
+          </div>
+        ))}
+
+      </section>
+    </div>
   );
 }
