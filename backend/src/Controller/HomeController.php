36d4cb1ebbf8d82c6cfa349adf3c90cdbfe47577
thomaslacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-10-18 21:28:56 +0200

    t-lac | Mix Back/Docker + Front existant

---
+++ backend/src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\SecurityBundle\Security;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class HomeController extends AbstractController
+{
+    private $security;
+    private $em;
+
+    public function __construct(Security $security, EntityManagerInterface $em)
+    {
+       $this->security = $security;
+         $this->em = $em;
+    }
+
+    public function index()
+    {
+        $users = $this->em->getRepository(User::class)->findAll();
+
+
+        return $this->render("base.html.twig",[
+
+            'users' => $users,
+        ]);
+    }
+
+}
