afa3093494dfbf8beee84a00f9a100215c21384d
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-10-21 11:27:35 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,8 @@ const buttonVariants = cva(
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        public: "bg-accent text-accent-foreground rounded-full",
+        private: "bg-destructive text-destructive-foreground rounded-full",
       },
       size: {
         default: "h-10 px-4 py-2",
