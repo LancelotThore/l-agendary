81aba930c781b66901de6f127461f283610e8711
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-10-21 09:25:04 +0200

    up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,13 +33,18 @@ export default function LoginPage() {
 
       <form action="" method='POST' className='flex justify-center items-center flex-col w-9/12 shadow-md mt-5 p-4 border border-FormBorder rounded-md md:w-2/4 md:p-6'>
         <h2 className={`${ralewaySemBold.className} text-base md:text-3xl md:text-center`}>Se connecter</h2>
-        <p className='text-xs md:text-base'>Rentrez votre mail pour que nous puissions vous envoyer un email de récupération.</p>
+        {/* <p className='text-xs md:text-base'>Rentrez votre mail pour que nous puissions vous envoyer un email de récupération.</p> */}
 
         <div className='w-full lg:w-96'>
           {inputs.map((input)=>(
             <div className='mt-3 flex flex-col gap-1'>
               <label className='text-xs md:text-base' htmlFor={input.id}>{input.name}</label>
               <Input id={input.id} placeholder={input.name} type={input.type} className='text-xs placeholder:text-FormBorder border-FormBorder md:text-base' />
+              {input.id === 'password' ? (
+                <>
+                  <a href="/recover/account" className='text-cardDate text-sm	text-right hover:underline hover:underline-offset-2 transition-transform'>Mot de passe oublié</a>
+                </>
+              ) : ''}
             </div>
           ))}
           
