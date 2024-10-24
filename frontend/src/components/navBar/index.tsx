2b52d834873a3a5c8563aceb4d7b08f275033d15
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-10-24 12:00:11 +0200

    navbar

--- frontend/src/components/navBar/index.tsx
+++ frontend/src/components/navBar/index.tsx
@@ -5,6 +5,10 @@ import { Logo, Search, MenuBurger, Home, Close, Profil, Calendar } from "../ui/i
 import { Button } from '../ui/button';
 import Link from 'next/link';
 
+
+const commonLinkClasses = "hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base";
+const buttonClasses = "hover:bg-primary/70";
+
 export function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
@@ -20,46 +24,33 @@ export function Navbar() {
                     <Link href="/" className="flex items-center">
                         <Logo className="w-40" />
                     </Link>
-                    {isUserLoggedIn ? (
-                        <>
-                            <Link href="/event" className="hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base">
-                                <span>Mes événements</span>
-                                <Calendar className="w-5" aria-hidden="true" />
-                            </Link>
-                            <Link
-                                href="/search"
-                                className="hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base"
-                            >
-                                <span>Rechercher</span>
-                                <Search className="w-5" aria-hidden="true" />
-                            </Link>
-                        </>
-                    ) : (
-                        <Link
-                            href="/search"
-                            className="hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base"
-                        >
-                            <span>Rechercher</span>
-                            <Search className="w-5" aria-hidden="true" />
+                    {isUserLoggedIn && (
+                        <Link href="/event" className={commonLinkClasses}>
+                            <span>Mes événements</span>
+                            <Calendar className="w-5" aria-hidden="true" />
                         </Link>
                     )}
+                    <Link href="/search" className={commonLinkClasses}>
+                        <span>Rechercher</span>
+                        <Search className="w-5" aria-hidden="true" />
+                    </Link>
                 </div>
                 <div className="hidden md:flex space-x-4 items-center">
                     {isUserLoggedIn ? (
                         <>
-                            <Link href="/profile" className="hidden md:flex items-center space-x-2 block py-2 px-3 rounded hover:bg-background font-medium text-base">
+                            <Link href="/profile" className={commonLinkClasses}>
                                 <span>Profil</span>
                                 <Profil className="w-8" aria-hidden="true" />
                             </Link>
-                            <Button className="hover:bg-primary/70" onClick={() => setIsUserLoggedIn(false)}>Se déconnecter</Button>
+                            <Button className={buttonClasses} onClick={() => setIsUserLoggedIn(false)}>Se déconnecter</Button>
                         </>
                     ) : (
                         <>
                             <Link href="/register">
-                                <Button className="hover:bg-primary/70">S'enregistrer</Button>
+                                <Button className={buttonClasses}>S'enregistrer</Button>
                             </Link>
                             <Link href="/login">
-                                <Button className="hover:bg-primary/70">Se connecter</Button>
+                                <Button className={buttonClasses}>Se connecter</Button>
                             </Link>
                         </>
                     )}
@@ -74,68 +65,56 @@ export function Navbar() {
             </div>
             <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-mobile">
                 <div className="font-medium flex flex-col py-4 rounded-lg bg-secondary items-end w-10/12 m-auto">
-                    <div>
-                        <Link
-                            href="/"
-                            className="flex items-center space-x-2 rtl:space-x-reverse block py-1 px-3 rounded font-medium text-xl"
-                        >
-                            <span>Accueil</span>
-                            <Home className="w-5" aria-hidden="true" />
+                    <Link
+                        href="/"
+                        className="flex items-center space-x-2 rtl:space-x-reverse block py-1 px-3 rounded font-medium text-xl"
+                    >
+                        <span>Accueil</span>
+                        <Home className="w-5" aria-hidden="true" />
+                    </Link>
+                    {isUserLoggedIn ? (
+
+                        <Link href="/event" className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl">
+                            <span>Mes événements</span>
+                            <Calendar className="w-5" aria-hidden="true" />
                         </Link>
-                    </div>
-                    <div>
-                        {isUserLoggedIn ? (
-                            <>
-                                <Link href="/event" className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl">
-                                    <span>Mes événements</span>
-                                    <Calendar className="w-5" aria-hidden="true" />
-                                </Link>
-                                <Link href="/search" className="flex items-center space-x-2 rtl:space-x-reverse block py-1 px-3 rounded font-medium text-xl">
-                                    <span>Rechercher</span>
-                                    <Search className="w-5" aria-hidden="true" />
-                                </Link>
-                
-                            </>
-                        ) : (
-                            <Link
-                                href="/search"
-                                className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl"
-                            >
-                                <span>Rechercher</span>
-                                <Search className="w-5" aria-hidden="true" />
-                            </Link>
-                        )}
-                    </div>
+
+                    ) : (
+                        <></>
+                    )}
+                    <Link
+                        href="/search"
+                        className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl"
+                    >
+                        <span>Rechercher</span>
+                        <Search className="w-5" aria-hidden="true" />
+                    </Link>
                     {isUserLoggedIn ? (
                         <>
-                            <div>
-                                <Link href="/profile" className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl">
-                                    <span>Profil</span>
-                                    <Profil className="w-8" aria-hidden="true" />
-                                </Link>
-                            </div>
-                            <div>
-                                <Button className="mt-2" onClick={() => setIsUserLoggedIn(false)}>Se déconnecter</Button>
-                            </div>
+                            <Link href="/profile" className="flex items-center space-x-2 rtl:space-x-reverse block py-2 px-3 rounded font-medium text-xl">
+                                <span>Profil</span>
+                                <Profil className="w-8" aria-hidden="true" />
+                            </Link>
+
+                            <Button className="mt-2" onClick={() => setIsUserLoggedIn(false)}>Se déconnecter</Button>
+
                         </>
                     ) : (
                         <>
-                            <div>
-                                <Link
-                                    href="/register"
-                                    className="flex items-center block px-3 py-1 rounded font-medium"
-                                >
-                                    <Button className="mt-2">S'enregistrer</Button>
-                                </Link>
-                            </div>
-                            <div>
-                                <Link
-                                    href="/login"
-                                    className="flex items-center block px-3 py-1 rounded font-medium"
-                                >
-                                    <Button className="mt-2">Se connecter</Button>
-                                </Link>
-                            </div>
+                            <Link
+                                href="/register"
+                                className="flex items-center block px-3 py-1 rounded font-medium"
+                            >
+                                <Button className="mt-2">S'enregistrer</Button>
+                            </Link>
+
+                            <Link
+                                href="/login"
+                                className="flex items-center block px-3 py-1 rounded font-medium"
+                            >
+                                <Button className="mt-2">Se connecter</Button>
+                            </Link>
+
                         </>
                     )}
                 </div>
