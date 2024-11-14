44740e9153caa40ef4dda61071fe976186fa93c4
LancelotThore <144225580+LancelotThore@users.noreply.github.com>
2024-11-14 23:52:57 +0100

    creation event

--- frontend/src/app/api/event.ts
+++ frontend/src/app/api/event.ts
@@ -9,7 +9,7 @@ export async function fetchEvents() {
   }
 }
 
-export async function fetchEvent(id) {
+export async function fetchEvent(id : number) {
   try {
     const res = await fetch("https://localhost/api/events/" + id);
     const data = await res.json();
@@ -20,7 +20,7 @@ export async function fetchEvent(id) {
   }
 }
 
-export async function fetchCreator(url) {
+export async function fetchCreator(url : string) {
   try {
     const res = await fetch("https://localhost" + url);
     const data = await res.json();
@@ -29,4 +29,49 @@ export async function fetchCreator(url) {
     console.error("Error fetching creator:", error);
     return null;
   }
+}
+
+export async function createEvent(
+    title: string,
+    description: string,
+    privacy: boolean,
+    location: string,
+    start_date: string,
+    end_date: string,
+    imageUrl: string,
+    creator: string
+) {
+    const formData = {
+        title,
+        description,
+        privacy,
+        location,
+        start_date,
+        end_date,
+        image: imageUrl,
+        creator
+    };
+
+    try {
+        const response = await fetch('https://localhost:443/api/events', {
+            method: 'POST',
+            headers: {
+                'accept': 'application/ld+json',
+                'Content-Type': 'application/ld+json'
+            },
+            body: JSON.stringify(formData),
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            const errorData = await response.json();
+            throw new Error(errorData.errors || 'Erreur lors de la création de l\'événement');
+        }
+    } catch (error) {
+        console.error('Error creating event:', error);
+        throw new Error('Erreur lors de la création de l\'événement');
+    }
 }
